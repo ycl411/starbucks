6ce6d0651f25380583959930dda9bb326274b193
ycl411 <ycl411@naver.com>
2022-06-24 10:07:38 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    //Logic..
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur',function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 
 // badge
 
@@ -158,6 +140,3 @@ spyEls.forEach( function (spyEl) {
 });
 
 
-// footer 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
